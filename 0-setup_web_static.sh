6d9a56f4e81d737a8c235136b76fa5d7905f901a
m0imaritim <moimaritim@gmail.com>
2024-05-06 15:49:07 -0700

    server setup

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# installs nginx if it doesn't exists and parent directories for web static deployment
+
+sudo apt update
+sudo apt install -y nginx
+
+sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared
+
+sudo echo "Web deploy" > /data/web_static/releases/test/index.html
+
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+sudo chown -R ubuntu:ubuntu /data/
+
+sudo sed -i '48i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default
+sudo service nginx restart
+exit 0
