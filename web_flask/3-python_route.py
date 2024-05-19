6d691aed2f0eada58096237702801a58054f199a
m0imaritim <moimaritim@gmail.com>
2024-05-19 01:16:26 -0700

    py text

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""module contains a minimalistic use of web framework flask"""
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_bnb():
+    """directing to home directory/page"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """directing to another page"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_route(text):
+    """directing to another page"""
+    text = text.replace('_', ' ')
+    return "C {}".format(text)
+
+
+@app.route("/python/", defaults={'text': 'is_cool'}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def py_route(text):
+    """directing to another page"""
+    text = text.replace('_', ' ')
+    return "Python {}".format(text)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
