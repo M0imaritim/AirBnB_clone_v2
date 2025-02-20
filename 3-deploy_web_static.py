3734ddc37cc14b832f9ea9d7582c7662748d6c25
Langat Moimaritim <moimaritim@gmail.com>
2025-02-20 05:44:03 -0800

    another fix

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -44,7 +44,6 @@ def do_deploy(archive_path):
         run("rm -rf {}/web_static".format(release_path))
         run("rm -rf /data/web_static/current")
         run("ln -s {} /data/web_static/current".format(release_path))
-        
 
         return True
     except:
